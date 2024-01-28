9bb11c175e030152ee05ed712cf16a2281f3dbb4
Earth_lojung <earth247god@gmail.com>
2024-01-28 22:46:58 +0700

    ADD PIC + INDEX

--- src/script.js
+++ src/script.js
@@ -1,13 +1,4 @@
-function toggleMenu() {
-    let menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
-}
 
-function animation() {
-    ScrollReveal().reveal('.headline' , { delay: 200 });
-    ScrollReveal().reveal('.body_center' , { delay: 400 });
-    ScrollReveal().reveal('.card' , { delay: 400 });
-    ScrollReveal().reveal('.footer' , { delay: 600 });
-}
 
-animation();
+
+
